abd68503f00805bbc4b48fa482ddc819021f3df9
Marawan Elzoeiry <140724136+MarawanElzoeiry@users.noreply.github.com>
2023-10-03 12:31:42 +0000

    Final

--- src/js/app.js
+++ src/js/app.js
@@ -29,23 +29,28 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  // Reset the website body with the new HTML output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+              ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>${(variables.name || "Name") +
+                " " +
+                (variables.lastName || "lastName")}</h1>
+              <h2>${variables.role || "Role"}</h2>
+              <h3>${variables.city || "City"}, ${variables.country ||
+    "Country"}</h3>
+              <ul class="${variables.socialMediaPosition || "position-right"}">
+                <li><a href="${variables.twitter ||
+                  ""}"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="${variables.github ||
+                  ""}"><i class="fab fa-github"></i></a></li>
+                <li><a href="${variables.linkedin ||
+                  ""}"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="${variables.instagram ||
+                  ""}"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>`;
 }
-
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
